69c20bdb965f2700acbea383c77465b9b12d1872
Christoph Welcz <christoph.welcz@codecentric.de>
2026-07-10 00:44:09 +0200

    ci: update coverage reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'node',
+    setupFiles: ['./test/setup.ts'],
     restoreMocks: true,
     clearMocks: true,
     coverage: {
-      reporter: ['text', 'json', 'html', 'json-summary'],
-      exclude: ['test/helpers/**', '**/*.test.ts', '**/*.config.ts', 'node_modules/**'],
+      reporter: ['text', 'json-summary', 'json', 'html'],
+      reportOnFailure: true,
+      exclude: ['test/helpers/**/*.ts'],
     },
   },
 })
