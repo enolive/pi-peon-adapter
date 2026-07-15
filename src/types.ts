6fc29d7a7d740ae6d7075b73369566700ad80c53
Christoph Welcz <christoph.welcz@codecentric.de>
2026-07-15 20:03:18 +0200

    🔥 remove workaround for https://github.com/gotgenes/pi-packages/pull/593

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+export { PERMISSIONS_UI_PROMPT_CHANNEL } from '@gotgenes/pi-permission-system'
+
 export type HookEvent =
   | 'SessionStart'
   | 'UserPromptSubmit'
@@ -19,9 +21,3 @@ export interface HookPayload {
 export interface PeonSink {
   send(payload: HookPayload): void
 }
-
-// workaround: due a wildcard import latest TS versions stopped resolving, importing it from the
-// @gotgenes/pi-permission-system package is not possible for now without major pain
-// node_modules/@gotgenes/pi-permission-system/src/rule.ts(1,33):
-//   error TS2307: Cannot find module '#src/path/path-flavor'
-export const PERMISSIONS_UI_PROMPT_CHANNEL = 'permissions:ui_prompt'
